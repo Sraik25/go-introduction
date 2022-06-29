3f2a9a32714cbc6e43e1b6cc6a8328826f14b026
Olek MP <olek.mp23@gmail.com>
2022-06-29 14:57:44 -0500

    04-modeling_data

---
+++ 04-modeling_data/internal/cli/beers.go
@@ -0,0 +1,44 @@
+package cli
+
+import (
+	"fmt"
+	"strconv"
+
+	beerscli "github.com/Sraik25/golang-introduction/04-modeling_data/internal"
+	"github.com/spf13/cobra"
+)
+
+const idFlag = "id"
+
+// InitBeersCmd initialize beers command
+func InitBeersCmd(repository beerscli.BeerRepo) *cobra.Command {
+	beersCmd := &cobra.Command{
+		Use:   "beers",
+		Short: "Print data about beers",
+		Run:   runBeersFn(repository),
+	}
+	beersCmd.Flags().StringP(idFlag, "i", "", "id of the beers")
+
+	return beersCmd
+}
+
+func runBeersFn(repository beerscli.BeerRepo) CobraFn {
+	return func(cmd *cobra.Command, args []string) {
+
+		beers, _ := repository.GetBeers()
+
+		id, _ := cmd.Flags().GetString(idFlag)
+
+		if id != "" {
+			i, _ := strconv.Atoi(id)
+			for _, beer := range beers {
+				if beer.ProductID == i {
+					fmt.Println(beer)
+					return
+				}
+			}
+		} else {
+			fmt.Println(beers)
+		}
+	}
+}
