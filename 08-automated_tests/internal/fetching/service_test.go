37adb917c569e14fd54bcb964567086fd8088b64
Olek MP <olek.mp23@gmail.com>
2022-07-01 21:57:36 -0500

    08-automated_tests-mocking

--- 08-automated_tests/internal/fetching/service_test.go
+++ 08-automated_tests/internal/fetching/service_test.go
@@ -4,40 +4,81 @@ import (
 	"errors"
 	"testing"
 
+	beerscli "github.com/Sraik25/golang-introduction/08-automated_tests/internal"
 	"github.com/Sraik25/golang-introduction/08-automated_tests/internal/fetching"
-	"github.com/Sraik25/golang-introduction/08-automated_tests/internal/storage/inmen"
+	"github.com/Sraik25/golang-introduction/08-automated_tests/internal/storage/mock"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestFetchByID(t *testing.T) {
 
 	tests := map[string]struct {
+		repo  beerscli.BeerRepo
 		input int
 		want  int
 		err   error
 	}{
-		"valid beer":     {input: 127, want: 127, err: nil},
-		"not found beer": {input: 999999, err: errors.New("error")},
+		"valid beer":            {repo: buildMockBeers(), input: 127, want: 127, err: nil},
+		"not found beer":        {repo: buildMockBeers(), input: 99999, err: errors.New("error")},
+		"error with repository": {repo: buildMockError(), err: errors.New("error")},
 	}
 
-	repo := inmen.NewRepository()
-	service := fetching.NewService(repo)
+	// repo := inmen.NewRepository()
+	// service := fetching.NewService(repo)
 
 	for name, tc := range tests {
 		t.Run(name, func(t *testing.T) {
+			service := fetching.NewService(tc.repo)
 			b, err := service.FetchByID(tc.input)
 
-			if err != nil && tc.err == nil {
-				t.Fatalf("not expected any errors and got %v", err)
+			if tc.err != nil {
+				assert.Error(t, err)
 			}
 
-			if err == nil && tc.err != nil {
-				t.Fatalf("expected an error and got nil")
+			if tc.err == nil {
+				assert.Nil(t, err)
 			}
 
-			if b.ProductID != tc.want {
-				t.Fatalf("expedted %d, got: %d ", tc.want, b.ProductID)
-			}
+			assert.Equal(t, tc.want, b.ProductID)
 		})
 	}
+}
 
+func buildMockBeers() beerscli.BeerRepo {
+	mockedRepo := &mock.BeerRepoMock{
+		GetBeersFunc: func() ([]beerscli.Beer, error) {
+			return []beerscli.Beer{
+				beerscli.NewBeer(
+					127,
+					"Mad Jack Mixer",
+					"Domestic Specialty",
+					"Molson",
+					"Canada",
+					beerscli.NewBeerType("Lager"),
+					23.95,
+				),
+				beerscli.NewBeer(
+					8520130,
+					"Grolsch 0.0",
+					"Non-Alcoholic Beer",
+					"Grolsch Export B.V.",
+					"Canada",
+					beerscli.NewBeerType("Non-Alcoholic Beer"),
+					49.50,
+				),
+			}, nil
+		},
+	}
+
+	return mockedRepo
+}
+
+func buildMockError() beerscli.BeerRepo {
+	mockedRepo := &mock.BeerRepoMock{
+		GetBeersFunc: func() ([]beerscli.Beer, error) {
+			return []beerscli.Beer{}, errors.New("error")
+		},
+	}
+
+	return mockedRepo
 }
